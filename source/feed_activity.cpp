2ac227b565f5f1df5e56ff418bb141b5bb9da082
metinxsezdin <metinxsezdin@gmail.com>
2026-08-07 02:08:43 +0300

    Map Login to BUTTON_Y and Refresh to BUTTON_LT

--- source/feed_activity.cpp
+++ source/feed_activity.cpp
@@ -230,7 +230,7 @@ brls::View* FeedActivity::createContentView()
         false, true);
 
     root->registerAction(
-        "Refresh", brls::BUTTON_Y,
+        "Refresh", brls::BUTTON_LT,
         [this](brls::View*) {
             video->flashStatus("refreshing...");
             restart();
@@ -243,7 +243,7 @@ brls::View* FeedActivity::createContentView()
         false);
 
     root->registerAction(
-        "Login", brls::BUTTON_START, [this](brls::View*) { openLogin(); return true; }, false,
+        "Login", brls::BUTTON_Y, [this](brls::View*) { openLogin(); return true; }, false,
         false);
 
     root->registerAction(
